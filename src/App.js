ba4a4342e1a871707f46568004b432ffe1ee3833
IsmailWARP <ismail@wearereasonablepeople.com>
2023-04-19 16:36:47 +0200

    tailwind fix and filestructure change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import './App.css';
+import SideMenu from "./components/SideMenu";
 
 function App() {
   return (
-    <div className="App bg-white">
-      <header className="App-header">
-        <p className='text-red-500'>ertetr</p>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <SideMenu />
+      <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+      <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
     </div>
   );
 }
---
+++ src/components/SideMenu.js
@@ -0,0 +1,5 @@
+export default function SideMenu () {
+    return (
+        <h1 className="text-red-300">Sidemenu</h1>
+    )
+}
