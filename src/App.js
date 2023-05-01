371a61996f0673ae4b41084524e13fbfb7b45a9e
IsmailWARP <ismail@wearereasonablepeople.com>
2023-05-01 17:11:14 +0200

    Fix: could not att multiple widgets

--- src/App.js
+++ src/App.js
@@ -83,11 +83,12 @@ function App() {
                 onDatasourceSave={ds => setDatasources([...datasources, ds])}
             />
             <div
+                ref={drop}
                 className="main-content w-screen h-screen"
                 style={{ backgroundColor }}
             >
                 {charts.length === 0 ? (
-                    <div ref={drop} className="grid grid-cols-4 h-screen gap-2 p-2">
+                    <div className="grid grid-cols-4 h-screen gap-2 p-2">
                         <strong className='absolute top-0 right-0 bottom-0 left-0 m-auto w-60 h-12'>Drag and drop a widget here.</strong>
                         <div className="bg-slate-800 bg-opacity-50 rounded-sm"></div>
                         <div className="bg-slate-800 bg-opacity-50 rounded-sm"></div>
